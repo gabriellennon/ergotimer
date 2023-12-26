54017ddaa41507f134c4711a484478be8d448f33
gabriel <gabriellennon7@gmail.com>
2023-12-26 11:59:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
